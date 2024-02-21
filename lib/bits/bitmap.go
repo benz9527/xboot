39adfbba86d448970c540931816a1d23e102c695
Ben.Zheng <benzheng2121@126.com>
2024-02-21 20:28:54 +0800

    feat: Added 32bit bitmap algorithm into lib.

---
+++ lib/bits/bitmap.go
@@ -0,0 +1,66 @@
+package bits
+
+import "bytes"
+
+type Bitmap interface {
+	SetBit(offset uint64, one bool) bool
+	GetBit(offset uint64) bool
+	GetBits() []byte
+	EqualTo(bm Bitmap) bool
+	Purge()
+}
+
+const (
+	maxBitMapSize = 1 << 32
+)
+
+type x32Bitmap struct {
+	bits []byte
+	size uint64
+}
+
+func NewX32Bitmap(size uint64) Bitmap {
+	if size <= 0 || size > maxBitMapSize {
+		size = maxBitMapSize
+	}
+	if remainder := size & 0x07; remainder != 0 {
+		size = size + (8 - remainder)
+	}
+	return &x32Bitmap{
+		bits: make([]byte, size>>3),
+		size: size - 1,
+	}
+}
+
+func (bm *x32Bitmap) SetBit(offset uint64, one bool) bool {
+	idx, pos := offset>>3, offset&0x07
+	if bm.size < offset {
+		return false
+	}
+	if !one {
+		bm.bits[idx] &= ^(1 << pos) // &^=
+	} else {
+		bm.bits[idx] |= 1 << pos
+	}
+	return true
+}
+
+func (bm *x32Bitmap) GetBit(offset uint64) bool {
+	idx, pos := offset>>3, offset&0x07
+	if bm.size < offset {
+		return false
+	}
+	return bm.bits[idx]>>pos != 0
+}
+
+func (bm *x32Bitmap) GetBits() []byte {
+	return bm.bits
+}
+
+func (bm *x32Bitmap) EqualTo(that Bitmap) bool {
+	return bytes.Compare(bm.GetBits(), that.GetBits()) == 0
+}
+
+func (bm *x32Bitmap) Purge() {
+	clear(bm.bits)
+}
