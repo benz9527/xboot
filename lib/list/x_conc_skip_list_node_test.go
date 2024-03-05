5b946218942c65a56a9293d424b8cb055be0030f
Ben.Zheng <benzheng2121@126.com>
2024-03-05 19:07:56 +0800

    draft: Migrated concurrent skip-list from JDK.

--- lib/list/x_conc_skip_list_node_test.go
+++ lib/list/x_conc_skip_list_node_test.go
@@ -1,26 +1,42 @@
 package list
 
-//func TestUnsafePointerCAS(t *testing.T) {
-//	a := &xConcurrentSkipListNode[uint8, *xSkipListObject]{
-//		weight: 1,
-//		object: &xSkipListObject{
-//			id: "1",
-//		},
-//		next: nil,
-//	}
-//	b := a
-//	c := &xConcurrentSkipListNode[uint8, *xSkipListObject]{
-//		weight: 2,
-//		object: &xSkipListObject{
-//			id: "2",
-//		},
-//		isMarker: false,
-//		next:     nil,
-//	}
-//
-//	ptr := atomic.Pointer[xConcurrentSkipListNode[uint8, *xSkipListObject]]{}
-//	ptr.Store(a)
-//	res := ptr.CompareAndSwap(b, c)
-//	require.True(t, res)
-//	require.Equal(t, c, ptr.Load())
-//}
+import (
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUnsafePointerCAS(t *testing.T) {
+	a := &xConcurrentSkipListNode[uint8, *xSkipListObject]{
+		weight: &atomic.Pointer[uint8]{},
+		object: &atomic.Pointer[*xSkipListObject]{},
+		next:   nil,
+	}
+	w1 := uint8(1)
+	o1 := &xSkipListObject{
+		id: "1",
+	}
+	a.weight.Store(&w1)
+	a.object.Store(&o1)
+
+	b := a
+
+	w2 := uint8(2)
+	o2 := &xSkipListObject{
+		id: "2",
+	}
+	c := &xConcurrentSkipListNode[uint8, *xSkipListObject]{
+		weight: &atomic.Pointer[uint8]{},
+		object: &atomic.Pointer[*xSkipListObject]{},
+		next:   nil,
+	}
+	c.weight.Store(&w2)
+	c.object.Store(&o2)
+
+	ptr := atomic.Pointer[xConcurrentSkipListNode[uint8, *xSkipListObject]]{}
+	ptr.Store(a)
+	res := ptr.CompareAndSwap(b, c)
+	require.True(t, res)
+	require.Equal(t, c, ptr.Load())
+}
