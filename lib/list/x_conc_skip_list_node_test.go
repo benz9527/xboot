a9c458b07ee572956a6f71c260e85b149c2b6792
Ben.Zheng <benzheng2121@126.com>
2024-03-07 10:46:07 +0800

    draft: Migrated concurrent skip-list from JDK.

--- lib/list/x_conc_skip_list_node_test.go
+++ lib/list/x_conc_skip_list_node_test.go
@@ -1,14 +1,25 @@
 package list
 
 import (
-	"github.com/stretchr/testify/require"
+	"runtime"
+	"strconv"
+	"sync"
 	"sync/atomic"
 	"testing"
+	"time"
 	"unsafe"
+
+	"github.com/stretchr/testify/require"
 )
 
+type xNode[W SkipListWeight, O HashObject] struct {
+	weight *atomic.Pointer[W]
+	object *atomic.Pointer[O]
+	next   *atomic.Pointer[xNode[W, O]]
+}
+
 func TestAtomicPointerCAS(t *testing.T) {
-	a := &xConcurrentSkipListNode[uint8, *xSkipListObject]{
+	a := &xNode[uint8, *xSkipListObject]{
 		weight: &atomic.Pointer[uint8]{},
 		object: &atomic.Pointer[*xSkipListObject]{},
 		next:   nil,
@@ -26,7 +37,7 @@ func TestAtomicPointerCAS(t *testing.T) {
 	o2 := &xSkipListObject{
 		id: "2",
 	}
-	c := &xConcurrentSkipListNode[uint8, *xSkipListObject]{
+	c := &xNode[uint8, *xSkipListObject]{
 		weight: &atomic.Pointer[uint8]{},
 		object: &atomic.Pointer[*xSkipListObject]{},
 		next:   nil,
@@ -34,7 +45,7 @@ func TestAtomicPointerCAS(t *testing.T) {
 	c.weight.Store(&w2)
 	c.object.Store(&o2)
 
-	ptr := atomic.Pointer[xConcurrentSkipListNode[uint8, *xSkipListObject]]{}
+	ptr := atomic.Pointer[xNode[uint8, *xSkipListObject]]{}
 	ptr.Store(a)
 	res := ptr.CompareAndSwap(b, c)
 	require.True(t, res)
@@ -56,4 +67,86 @@ func TestUnsafePointerCAS(t *testing.T) {
 	res := atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(&a)), unsafe.Pointer(b), unsafe.Pointer(c))
 	require.True(t, res)
 	require.Equal(t, c, a)
+}
+
+func TestUnsafePointerCAS_ConcurrentMemVisibility(t *testing.T) {
+	runtime.GOMAXPROCS(4)
+	type obj struct {
+		id string
+	}
+	a := &obj{
+		id: "1",
+	}
+	b := a
+	c := &obj{
+		id: "2",
+	}
+
+	logC := make(chan string, 1000)
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+	go func() {
+		for i := 0; i < 200; i++ {
+			val := atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&a)))
+			log := "cas load a val, obj: " + (*obj)(val).id + ", id: 1"
+			runtime.Gosched()
+			logC <- log
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 200; i++ {
+			val := atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&a)))
+			log := "cas load a val, obj: " + (*obj)(val).id + ", id: 2"
+			runtime.Gosched()
+			logC <- log
+		}
+		wg.Done()
+	}()
+	go func() {
+		swapped := false
+		for i := 0; i < 50; i++ {
+			ms := cryptoRandInt32() % 11
+			if ms == 5 && !swapped {
+				log := "starting to cas obj to c, id: 1, loop: " + strconv.Itoa(i)
+				logC <- log
+				swapped = atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(&a)), unsafe.Pointer(b), unsafe.Pointer(c))
+				if swapped != true {
+					log = "cas result is false, id: 1"
+					logC <- log
+				} else {
+					log = "cas result is ok, id: 1"
+					logC <- log
+				}
+			}
+		}
+		wg.Done()
+	}()
+	go func() {
+		swapped := false
+		for i := 0; i < 50; i++ {
+			ms := cryptoRandInt32() % 11
+			if ms == 6 && !swapped {
+				log := "starting to cas obj to c, id: 2, loop: " + strconv.Itoa(i)
+				logC <- log
+				swapped = atomic.CompareAndSwapPointer((*unsafe.Pointer)(unsafe.Pointer(&a)), unsafe.Pointer(b), unsafe.Pointer(c))
+				if swapped != true {
+					log = "cas result is false, id: 2"
+					logC <- log
+				} else {
+					log = "cas result is ok, id: 2"
+					logC <- log
+				}
+			}
+		}
+		wg.Done()
+	}()
+	go func() {
+		for log := range logC {
+			t.Log(log)
+		}
+	}()
+	wg.Wait()
+	time.Sleep(1 * time.Second)
 }
