5c31f6ea2466f20927256e897230c7c718e8d247
Ben.Zheng <benzheng2121@126.com>
2024-03-27 20:29:54 +0800

    draft: Implemented the arena.

--- lib/list/x_conc_skl_arena.go
+++ lib/list/x_conc_skl_arena.go
@@ -1,10 +1,76 @@
 package list
 
 import (
+	"unsafe"
+
 	"github.com/benz9527/xboot/lib/infra"
 )
 
-// FIXME: How to recycle the x-conc-skl node and indices and avoid the data race?
+// References:
+// https://github.com/ortuman/nuke
+// https://github.com/dgraph-io/badger/blob/master/skl/arena.go
+
+type xConcSklBuffer struct {
+	ptr        unsafe.Pointer
+	offset     uintptr // current index offset
+	maxCount   uintptr // max element count
+	eSize      uintptr // fixed element size
+	eAlignment uintptr // fixed element alignment
+}
+
+func (buf *xConcSklBuffer) availableBytes() uintptr {
+	return buf.maxCount*buf.eSize - buf.offset
+}
+
+func (buf *xConcSklBuffer) alloc() (unsafe.Pointer, bool) {
+	if /* lazy init */ buf.ptr == nil {
+		buffer := make([]byte, buf.maxCount*buf.eSize)
+		buf.ptr = unsafe.Pointer(unsafe.SliceData(buffer))
+	}
+	alignOffset := uintptr(0)
+	for alignedPtr := uintptr(buf.ptr) + buf.offset; alignedPtr%buf.eAlignment != 0; alignedPtr++ {
+		alignOffset++
+	}
+	allocSize := buf.eSize + alignOffset
+
+	if /* scale */ buf.availableBytes() < allocSize {
+		return nil, false
+	}
+
+	ptr := unsafe.Pointer(uintptr(buf.ptr) + buf.offset + alignOffset)
+	buf.offset += allocSize
+
+	// Translated into runtime.memclrNoHeapPointers by compiler.
+	// An assembler optimized implementation.
+	// go/src/runtime/memclr_$GOARCH.s (since https://codereview.appspot.com/137880043)
+	bytes := unsafe.Slice((*byte)(ptr), buf.eSize)
+	for i := range bytes {
+		bytes[i] = 0
+	}
+	return ptr, true
+}
+
+func (buf *xConcSklBuffer) reset() {
+	if buf.offset == 0 {
+		return
+	}
+	// Overwrite allow
+	buf.offset = 0
+}
+
+func (buf *xConcSklBuffer) free() {
+	buf.reset()
+	buf.ptr = nil
+}
+
+func newXConcSklBuffer(elements, size, alignment uintptr) *xConcSklBuffer {
+	return &xConcSklBuffer{
+		maxCount:   elements,
+		eSize:      size,
+		eAlignment: alignment,
+		offset:     uintptr(0),
+	}
+}
 
 // The pool is used to recycle the auxiliary data structure.
 type xConcSklArena[K infra.OrderedKey, V any] struct {
--- lib/list/x_conc_skl_arena_test.go
+++ lib/list/x_conc_skl_arena_test.go
@@ -1,1 +1,30 @@
 package list
+
+import (
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestXConcSklBuffer(t *testing.T) {
+	obj := xSklObject{}
+	buffer := newXConcSklBuffer(100, unsafe.Sizeof(obj), unsafe.Alignof(obj))
+	optr, ok := buffer.alloc()
+	require.True(t, ok)
+	(*xSklObject)(optr).id = "abc"
+	o := *(*xSklObject)(optr)
+	t.Log(o)
+	op := &o
+	op2 := (*xSklObject)(optr)
+	t.Logf("%p, %p, %p\n", op, op2, (*xSklObject)(optr))
+
+	optr2, ok := buffer.alloc()
+	require.True(t, ok)
+	(*xSklObject)(optr2).id = "bcd"
+	o2 := *(*xSklObject)(optr2)
+	t.Log(o2)
+	op_2 := &o2
+	op_2_1 := (*xSklObject)(optr2)
+	t.Logf("%p, %p, %p\n", op_2, op_2_1, (*xSklObject)(optr2))
+}
