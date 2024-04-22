e9be141a63a42885c9e6506270b793fa20b49b4e
Ben.Zheng <benzheng2121@126.com>
2024-04-22 15:29:34 +0100

    test: Added swiss-map, thread-safe-map, map and sync-map benchmark tests. (#23)

--- lib/kv/thread_safe_map_test.go
+++ lib/kv/thread_safe_map_test.go
@@ -6,6 +6,7 @@ import (
 	randv2 "math/rand/v2"
 	"strconv"
 	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -56,21 +57,38 @@ func TestThreadSafeMap_SimpleCRUD(t *testing.T) {
 
 func BenchmarkStringThreadSafeMap(b *testing.B) {
 	const strKeyLen = 8
-	sizes := []int{16, 128, 1024, 8192, 131072}
+	sizes := []int{16, 128, 1024, 8192, 131072, 1 << 20}
 	for _, n := range sizes {
-		b.Run("n="+strconv.Itoa(n), func(bb *testing.B) {
+		b.Run("ThreadSafeMap n="+strconv.Itoa(n), func(bb *testing.B) {
 			keys := genStrKeys(strKeyLen, n)
 			n := uint32(len(keys))
 			mod := n - 1 // power of 2 fast modulus
 			require.Equal(bb, 1, bits.OnesCount32(n))
 			m := NewThreadSafeMap[string, string](WithThreadSafeMapInitCap[string, string](n))
+			bb.ResetTimer()
 			for _, k := range keys {
 				_ = m.AddOrUpdate(k, k)
 			}
-			bb.ResetTimer()
 			var ok bool
 			for i := 0; i < b.N; i++ {
 				_, ok = m.Get(keys[uint32(i)&mod])
+				require.True(b, ok)
+			}
+			bb.ReportAllocs()
+		})
+		b.Run("SyncMap n="+strconv.Itoa(n), func(bb *testing.B) {
+			keys := genStrKeys(strKeyLen, n)
+			n := uint32(len(keys))
+			mod := n - 1 // power of 2 fast modulus
+			require.Equal(bb, 1, bits.OnesCount32(n))
+			m := sync.Map{}
+			bb.ResetTimer()
+			for _, k := range keys {
+				m.Store(k, k)
+			}
+			var ok bool
+			for i := 0; i < b.N; i++ {
+				_, ok = m.Load(keys[uint32(i)&mod])
 				require.True(b, ok)
 			}
 			bb.ReportAllocs()
@@ -81,23 +99,41 @@ func BenchmarkStringThreadSafeMap(b *testing.B) {
 func BenchmarkThreadSafeMapReadWrite(b *testing.B) {
 	value := []byte(`abc`)
 	for i := 0; i <= 10; i++ {
-		b.Run(fmt.Sprintf("frac_%d", i), func(bb *testing.B) {
+		b.Run(fmt.Sprintf("ThreadSafeMap frac_%d", i), func(bb *testing.B) {
 			readFrac := float32(i) / 10.0
 			tsm := NewThreadSafeMap[int, []byte](
 				WithThreadSafeMapInitCap[int, []byte](4096),
 			)
 			bb.ResetTimer()
-			var count int
+			count := atomic.Int32{}
 			bb.RunParallel(func(pb *testing.PB) {
 				for pb.Next() {
 					if randv2.Float32() < readFrac {
 						v, exists := tsm.Get(randv2.Int())
 						if exists && v != nil {
-							count++
+							count.Add(1)
 						}
 					} else {
 						err := tsm.AddOrUpdate(randv2.Int(), value)
 						require.NoError(bb, err)
+					}
+				}
+			})
+		})
+		b.Run(fmt.Sprintf("SyncMap frac_%d", i), func(bb *testing.B) {
+			readFrac := float32(i) / 10.0
+			tsm := sync.Map{}
+			bb.ResetTimer()
+			count := atomic.Int32{}
+			bb.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					if randv2.Float32() < readFrac {
+						v, exists := tsm.Load(randv2.Int())
+						if exists && v != nil {
+							count.Add(1)
+						}
+					} else {
+						tsm.Store(randv2.Int(), value)
 					}
 				}
 			})
@@ -107,9 +143,9 @@ func BenchmarkThreadSafeMapReadWrite(b *testing.B) {
 
 func BenchmarkStringThreadSafeMap_DataRace(b *testing.B) {
 	const strKeyLen = 8
-	sizes := []int{16, 128, 1024, 8192, 131072}
+	sizes := []int{16, 128, 512, 1024, 8192, 131072, 1 << 19}
 	for _, n := range sizes {
-		b.Run("n="+strconv.Itoa(n), func(bb *testing.B) {
+		b.Run("ThreadSafeMap n="+strconv.Itoa(n), func(bb *testing.B) {
 			keys := genStrKeys(strKeyLen, n)
 			n := uint32(len(keys))
 			mod := n - 1 // power of 2 fast modulus
@@ -156,6 +192,57 @@ func BenchmarkStringThreadSafeMap_DataRace(b *testing.B) {
 			var ok bool
 			for i := 0; i < b.N; i++ {
 				_, ok = m.Get(keys[uint32(i)&mod])
+				require.True(b, ok)
+			}
+			bb.ReportAllocs()
+		})
+		b.Run("syncMap n="+strconv.Itoa(n), func(bb *testing.B) {
+			keys := genStrKeys(strKeyLen, n)
+			n := uint32(len(keys))
+			mod := n - 1 // power of 2 fast modulus
+			require.Equal(bb, 1, bits.OnesCount32(n))
+			m := sync.Map{}
+			channels := make([]chan string, 4)
+			for i := 0; i < len(channels); i++ {
+				channels[i] = make(chan string)
+			}
+			wg := sync.WaitGroup{}
+			wg.Add(4)
+			bb.ResetTimer()
+			go func() {
+				for k := range channels[0] {
+					m.Store(k, k)
+				}
+				wg.Done()
+			}()
+			go func() {
+				for k := range channels[1] {
+					m.Store(k, k)
+				}
+				wg.Done()
+			}()
+			go func() {
+				for k := range channels[2] {
+					m.Store(k, k)
+				}
+				wg.Done()
+			}()
+			go func() {
+				for k := range channels[3] {
+					m.Store(k, k)
+				}
+				wg.Done()
+			}()
+			for i, k := range keys {
+				channels[i%4] <- k
+			}
+			for i := 0; i < 4; i++ {
+				close(channels[i])
+			}
+			wg.Wait()
+			var ok bool
+			for i := 0; i < b.N; i++ {
+				_, ok = m.Load(keys[uint32(i)&mod])
 				require.True(b, ok)
 			}
 			bb.ReportAllocs()
