6856ede696482990a0a0a1fa7fbc7ce31f99edaf
Ben.Zheng <benzheng2121@126.com>
2024-04-23 10:10:37 +0800

    test: Updated id package test coverage.

--- lib/id/monotonic_id_test.go
+++ lib/id/monotonic_id_test.go
@@ -1,18 +1,24 @@
 package id
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestMonotonicNonZeroID(t *testing.T) {
+	id := new(monotonicNonZeroID)
+	id.val = math.MaxUint64
+	require.Equal(t, uint64(1), id.next())
+
 	gen, err := MonotonicNonZeroID()
 	require.Nil(t, err)
 	require.Equal(t, uint64(1), gen.Number())
 	for i := 0; i < 1000; i++ {
 		require.Less(t, gen.Number(), gen.Number())
 	}
+	require.NotEqual(t, gen.Str(), gen.Str())
 }
 
 func BenchmarkMonotonicNonZeroID(b *testing.B) {
