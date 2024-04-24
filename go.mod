27a8c3e40f890ddd03208807f30536016d4c9bdf
Ben.Zheng <benzheng2121@126.com>
2024-04-24 18:51:39 +0800

    test: Added go-redis package to test xlog.

--- go.mod
+++ go.mod
@@ -22,9 +22,12 @@ require (
 )
 
 require (
+	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
+	github.com/alicebob/miniredis/v2 v2.32.1
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
@@ -35,9 +38,11 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.52.2 // indirect
 	github.com/prometheus/procfs v0.13.0 // indirect
+	github.com/redis/go-redis/v9 v9.5.1
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/tklauser/go-sysconf v0.3.13 // indirect
 	github.com/tklauser/numcpus v0.7.0 // indirect
+	github.com/yuin/gopher-lua v1.1.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	go.opentelemetry.io/otel/sdk v1.24.0 // indirect
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
--- xlog/goredis_test.go
+++ xlog/goredis_test.go
@@ -3,7 +3,12 @@ package xlog
 import (
 	"context"
 	"testing"
+	"time"
 
+	mredisv2 "github.com/alicebob/miniredis/v2"
+	redisv9 "github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
@@ -24,11 +29,50 @@ func TestGoRedisXLogger(t *testing.T) {
 	}
 	parentLogger = NewXLogger(opts...)
 	logger = NewGoRedisXLogger(parentLogger)
-	parentLogger.IncreaseLogLevel(zapcore.InfoLevel)
+	parentLogger.IncreaseLogLevel(zapcore.ErrorLevel)
 	parentLogger.Debug("abc")
 	logger.Printf(context.TODO(), "test %d", 123)
 	parentLogger.IncreaseLogLevel(zapcore.DebugLevel)
 	parentLogger.Debug("abc")
 	logger.Printf(context.TODO(), "test %d", 123)
+	_ = parentLogger.Sync()
+}
+
+func TestGoRedisXLogger_MiniRedis(t *testing.T) {
+	var (
+		parentLogger XLogger         = nil
+		logger       *GoRedisXLogger = nil
+	)
+	opts := []XLoggerOption{
+		WithXLoggerLevel(LogLevelDebug),
+		WithXLoggerEncoder(JSON),
+		WithXLoggerWriter(StdOut),
+		WithXLoggerConsoleCore(),
+		WithXLoggerTimeEncoder(zapcore.ISO8601TimeEncoder),
+		WithXLoggerLevelEncoder(zapcore.CapitalLevelEncoder),
+	}
+	parentLogger = NewXLogger(opts...)
+	logger = NewGoRedisXLogger(parentLogger)
+
+	const addr = "127.0.0.1:6379"
+	mredis := mredisv2.NewMiniRedis()
+	defer func() { mredis.Close() }()
+	go func() {
+		err := mredis.StartAddr(addr)
+		require.NoError(t, err)
+	}()
+
+	redisv9.SetLogger(logger)
+	rclient := redisv9.NewClient(&redisv9.Options{
+		Addr:     addr,
+		Password: "",
+		DB:       0,
+	})
+	defer func() { _ = rclient.Close() }()
+	rclient.Set(context.TODO(), "abc", "123", 0)
+	cmd := rclient.BLPop(context.TODO(), 1*time.Millisecond, "abc")
+	_, err := cmd.Result()
+	require.Error(t, err)
+	parentLogger.Debug("go redis", zap.Any("cmd", cmd.String()))
 	_ = parentLogger.Sync()
 }
